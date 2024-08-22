9c20c3b0a1a286fb70748eff95b9e933199a1ce1
yasser-osamaa <yasserosamaa88@gmail.com>
2024-08-22 21:15:42 +0300

    create custom app bar and theme

---
+++ lib/widgets/custom_search_icon.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(.05),
+      ),
+      child: const Icon(
+        Icons.search,
+        size: 30,
+      ),
+    );
+  }
+}
---
+++ lib/widgets/notes_view_body.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/custom_appbar.dart';
+
+class NotesViewBody extends StatelessWidget {
+  const NotesViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          CustomAppBar(),
+        ],
+      ),
+    );
+  }
+}
