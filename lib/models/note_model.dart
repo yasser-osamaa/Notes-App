983f17d86402546e1f7420ac071cb7c9d7df63a3
yasser-osamaa <yasserosamaa88@gmail.com>
2024-08-23 17:00:23 +0300

    add note model

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.date,
+      required this.color,
+      required this.title,
+      required this.subTitle});
+}
--- lib/models/notes_model.dart
+++
@@ -1,6 +0,0 @@
-class NotesModel {
-  final String title;
-  final String content;
-
-  NotesModel({required this.title, required this.content});
-}
