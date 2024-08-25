03fdb7a12472d6c5ce93abd6b3fa8d151222c748
yasser-osamaa <yasserosamaa88@gmail.com>
2024-08-25 16:15:14 +0300

    edit form sheet to be fixed size with keyboard

--- lib/widgets/add_note_bottom_sheet.dart
+++ lib/widgets/add_note_bottom_sheet.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:notes_app/cubits/add_notes_cubit/add_notes_cubit.dart';
 import 'package:notes_app/cubits/add_notes_cubit/add_notes_states.dart';
 import 'package:notes_app/widgets/add_note_form.dart';
@@ -26,9 +25,13 @@ class AddNoteBottomSheet extends StatelessWidget {
           return SingleChildScrollView(
             child: AbsorbPointer(
               absorbing: state is AddNoteLoading ? true : false,
-              child: const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16),
-                child: AddNoteForm(),
+              child: Padding(
+                padding: EdgeInsets.only(
+                  right: 16,
+                  left: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom,
+                ),
+                child: const AddNoteForm(),
               ),
             ),
           );
